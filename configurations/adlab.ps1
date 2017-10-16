d73d0bdb81463d986aded4115ff44eaeb1e52863
krjhitch <krjhitch@gmail.com>
2017-10-16 15:19:52 -0400

    added join

--- configurations/adlab.ps1
+++ configurations/adlab.ps1
@@ -1,53 +1,79 @@
 configuration CreateFirstDC { 
     param ( 
-         [Parameter(Mandatory)][String] $DomainName,
-         [Parameter(Mandatory)][System.Management.Automation.PSCredential] $Admincreds,
-         [Int]$RetryCount       = 6,
-         [Int]$RetryIntervalSec = 10
-     ) 
-     Import-DscResource -ModuleName PSDesiredStateConfiguration, xActiveDirectory
+        [Parameter(Mandatory)][String] $DomainName,
+        [Parameter(Mandatory)][System.Management.Automation.PSCredential] $Admincreds,
+        [Int]$RetryCount = 6,
+        [Int]$RetryIntervalSec = 10
+    ) 
+    Import-DscResource -ModuleName PSDesiredStateConfiguration, xActiveDirectory
      
-     [System.Management.Automation.PSCredential] $DomainCreds = New-Object System.Management.Automation.PSCredential("${DomainName}\$($Admincreds.UserName)", $Admincreds.Password)
+    [System.Management.Automation.PSCredential] $DomainCreds = New-Object System.Management.Automation.PSCredential("${DomainName}\$($Admincreds.UserName)", $Admincreds.Password)
  
-     node 'localhost' {
-         WindowsFeature 'ADDSInstall' { 
-             Name = "AD-Domain-Services" 
-         } 
+    node 'localhost' {
+        WindowsFeature 'ADDSInstall' { 
+            Name = "AD-Domain-Services" 
+        } 
  
-         xADDomain 'FirstDC' {
-             DomainName                    = $DomainName
-             DomainAdministratorCredential = $DomainCreds
-             SafemodeAdministratorPassword = $DomainCreds
-             DependsOn                     = "[WindowsFeature]ADDSInstall"
-         } 
+        xADDomain 'FirstDC' {
+            DomainName                    = $DomainName
+            DomainAdministratorCredential = $DomainCreds
+            SafemodeAdministratorPassword = $DomainCreds
+            DependsOn                     = "[WindowsFeature]ADDSInstall"
+        } 
  
-         LocalConfigurationManager {
-             ConfigurationMode  = 'ApplyOnly'
-             RebootNodeIfNeeded = $true
-         }
-     }
- }
+        LocalConfigurationManager {
+            ConfigurationMode  = 'ApplyOnly'
+            RebootNodeIfNeeded = $true
+        }
+    }
+}
  
- configuration AddAdditionalDCs {
+configuration AddAdditionalDC {
     param ( 
-         [Parameter(Mandatory)][String] $DomainName,
-         [Parameter(Mandatory)][System.Management.Automation.PSCredential] $Admincreds
+        [Parameter(Mandatory)][String] $DomainName,
+        [Parameter(Mandatory)][System.Management.Automation.PSCredential] $Admincreds
     ) 
     Import-DscResource -ModuleName PSDesiredStateConfiguration, xActiveDirectory
      
     [System.Management.Automation.PSCredential] $DomainCreds = New-Object System.Management.Automation.PSCredential ("${DomainName}\$($Admincreds.UserName)", $Admincreds.Password)
  
-     node 'localhost' {
-         xADDomainController 'AdditionalDC' {
-             DomainName = $DomainName
-             DomainAdministratorCredential = $DomainCreds
-             SafemodeAdministratorPassword = $DomainCreds
-         }
+    node 'localhost' {
+        WindowsFeature 'ADDSInstall' { 
+            Name = "AD-Domain-Services" 
+        } 
+
+        xADDomainController 'AdditionalDC' {
+            DomainName                    = $DomainName
+            DomainAdministratorCredential = $DomainCreds
+            SafemodeAdministratorPassword = $DomainCreds
+            DependsOn                     = "[WindowsFeature]ADDSInstall"
+        }
  
-         LocalConfigurationManager 
-         {
-             ConfigurationMode = 'ApplyOnly'
-             RebootNodeIfNeeded = $true
-         }
+        LocalConfigurationManager {
+            ConfigurationMode  = 'ApplyOnly'
+            RebootNodeIfNeeded = $true
+        }
     }
- } 
+} 
+
+configuration JoinAD { 
+    param ( 
+        [Parameter(Mandatory)][String] $DomainName,
+        [Parameter(Mandatory)][System.Management.Automation.PSCredential] $Admincreds
+    ) 
+    Import-DscResource -ModuleName PSDesiredStateConfiguration, xActiveDirectory, xDSCDomainJoin
+    
+    [System.Management.Automation.PSCredential ]$DomainCreds = New-Object System.Management.Automation.PSCredential ("${DomainName}\$($Admincreds.UserName)", $Admincreds.Password)
+
+    node 'localhost'  {
+        xDSCDomainJoin "Join$Domain" {
+            Domain     = $DomainName
+            Credential = $DomainCreds
+        }
+
+        LocalConfigurationManager {
+            ConfigurationMode  = 'ApplyOnly'
+            RebootNodeIfNeeded = $true
+        }
+    }
+} 
